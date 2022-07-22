491521b3022f389454519ada97cd60730d1267d8
DeangeloBowen1 <deangelo.bowen15@gmail.com>
2022-07-22 11:39:14 -0500

    additionals

--- Deangelo Scratch/prepare.py
+++ Deangelo Scratch/prepare.py
@@ -72,8 +72,8 @@ def remove_stopwords(string, extra_words=[], exclude_words=[]):
                      "build", "status", 'sup', "introduction", "intro", "developer", "samplecode",
                     "user", "experience", "400", "200", "100", "&#9;", "kbd", "td", "tr", "org", 
                     "wiki", "wikipedia", "3rdparty", "license", "0", "version", "maximkulkin", "22", 
-                    "33","44", "p", "e", "printername", "nsbp", "touch", "id", "error", "working", 
-                    "green", "githubusercontent", "45159366", ]
+                    "33","44", "p", "e", "printername", "nbsp", "touch", "id", "error", "working", 
+                    "green", "githubusercontent", "45159366", "waifu2x" ]
     stop_words.extend(new_stopwords)
     
     #Add extra words to be removed to the stop word list
